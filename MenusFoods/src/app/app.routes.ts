747ab3f5e14e849a623eb1db1b35ec7f7b96e22b
JoaoPTavare <jjoaopedrotava05@gmail.com>
2023-04-08 17:13:27 -0300

    eviando

---
+++ MenusFoods/src/app/app.routes.ts
@@ -0,0 +1,17 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: 'home',
+    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'produtos',
+    loadComponent: () => import('./produtos/produtos.page').then( m => m.ProdutosPage)
+  },
+];
